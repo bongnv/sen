94bf91326f03691aa17b305fcce267f0afca0cfd
Bong Nguyen <vanbong@gmail.com>
2023-05-28 20:26:21 +1000

    docs: add an example of a hello service

---
+++ examples/hello/go.mod
@@ -0,0 +1,27 @@
+module github.com/bongnv/examples/hello
+
+go 1.20
+
+require (
+	github.com/bongnv/sen/pkg/plugins/echo v0.1.2
+	github.com/bongnv/sen/pkg/plugins/zap v0.1.2
+	github.com/bongnv/sen/pkg/sen v0.1.0
+	github.com/labstack/echo/v4 v4.10.2
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/caarlos0/env/v8 v8.0.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
---
+++ examples/hello/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	echoPlugin "github.com/bongnv/sen/pkg/plugins/echo"
+	zapPlugin "github.com/bongnv/sen/pkg/plugins/zap"
+	"github.com/bongnv/sen/pkg/sen"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+func main() {
+	app := sen.New()
+	err := app.With(
+		sen.GracefulShutdown(),
+		&zapPlugin.Plugin{},
+		echoPlugin.Bundle(),
+		&Service{},
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to initialize the app due to %v\n", err)
+	}
+
+	if err := app.Run(context.Background()); err != nil {
+		log.Printf("Error while running, err: %v\n", err)
+	}
+}
+
+// Service is an example implementation of a service.
+// Its dependencies are injected when registering to sen.
+type Service struct {
+	Injector sen.Injector  `inject:"injector"`
+	Echo     *echo.Echo    `inject:"echo"`
+	Logger   *zap.Logger   `inject:"logger"`
+	LC       sen.Lifecycle `inject:"lifecycle"`
+}
+
+// Initialize initializes the service.
+// It also implements sen.Plugin interface so
+// it can be used as a plugin.
+func (s *Service) Initialize() error {
+	s.Logger.Info("The service is initializing")
+
+	// Registering handlers with echo.Echo.
+	// Following is an example of registering the handler for GET /hello
+	s.Echo.GET("/hello", s.Hello)
+
+	// Registers hooks at OnRun and OnShutdown.
+	// Not all services need these.
+	s.LC.OnRun(s.Run)
+	s.LC.OnShutdown(s.Shutdown)
+
+	// Registering the service under the name "my-service" so
+	// it can be injected as a dependency later on.
+	return s.Injector.Register("my-service", s)
+}
+
+// Run is a hook when the application starts to run.
+// If is a long-running service, it should block the function from returning
+// until it finishes.
+//
+// For example, echo.Plugin will block its OnRun hook until the HTTP service stops.
+func (s *Service) Run(_ context.Context) error {
+	s.Logger.Info("The service is starting")
+	return nil
+}
+
+// Shutdown is a hook when the application is shutting down.
+func (s *Service) Shutdown(_ context.Context) error {
+	s.Logger.Info("The service is shutting down")
+	return nil
+}
+
+// Hello is a handler that handles requests to GET /hello.
+func (s *Service) Hello(c echo.Context) error {
+	s.Logger.Info("Hello is called")
+	return c.String(http.StatusOK, "OK")
+}
